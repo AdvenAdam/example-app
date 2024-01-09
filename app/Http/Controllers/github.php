03c466e015fbee558c8f7d27c9a64d511c14c941
AdvenAdam <advenadam@student.uns.ac.id>
2024-01-09 17:10:55 +0700

    fix url and add return as json

--- app/Http/Controllers/github.php
+++ app/Http/Controllers/github.php
@@ -9,16 +9,23 @@ class github extends Controller
 {
     function index() {
 
-        $token = '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'; // Replace with your GitHub personal access token
+        $token = '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'; 
         $client = new Client([
             'headers' => [
                 'Authorization' => 'Bearer ' . $token,
             ],
         ]);
-        $response = $client->get('https://api.github.com/AdvenAdam/repos');
-        dd($response);
+        $response = $client->get('https://api.github.com/users/AdvenAdam/repos');
         $repositories = json_decode($response->getBody());
-        dd($repositories);
-        return view('github.index', compact('repositories'));
+        $returnArray = [];
+        foreach ($repositories as $repo) {
+            $data = [
+                'name' => $repo->name,
+                'created_at' => $repo->created_at,
+                'url' => $repo->html_url
+            ];
+            array_push($returnArray, $data);
+        }
+        return json_encode($returnArray);
     }
-    }
+}
